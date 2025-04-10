5cb2988ddca5f4ea2fef6e8669f94f50c7c8bd37
pedrorodriguesx <pedrinbarrildeuranio@gmail.com>
2025-04-10 08:45:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var país = ["África do Sul", "Angola", "Egito", "Marrocos","Nigéria", "Senega", "Quênia", 
+var país = ["África do Sul", "Angola", "Egito", "Brasil","Nigéria", "Senega", "Quênia", 
 "Sudão", "Uganda", "Moçambique", "Guiné",]
 
 var paísEscolhido = parseInt(prompt("Digite um número de 0 a 10"))

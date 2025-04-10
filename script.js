a93bc1be1a59fe1df0711dfd2eae16cceee777b2
pedrorodriguesx <pedrinbarrildeuranio@gmail.com>
2025-04-10 08:32:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var país = ["África do Sul", "Angola", "Egito", "Marrocos","Nigéria", "Senega", "Quênia", 
+"Sudão", "Uganda", "Moçambique", "Guiné",]
+
+var paísEscolhido = parseInt(prompt("Digite um número de 0 a 10"))
+
+document.write("País escolhido: " +  país[paísEscolhido])
